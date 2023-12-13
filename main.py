42eb9976844e47396d3c6ad796cf964860ca1c9e
Reethika111 <138588697+Reethika111@users.noreply.github.com>
2023-12-13 16:34:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from parsing import *
+import base
+
+
+def get_grammar():
+    return base.get_sample_1()
+
+
+def describe_grammar(gr):
+    return '\n'.join([
+        'Indexed grammar rules (%d in total):' % len(gr.productions),
+        str(gr) + '\n',
+        'Grammar non-terminals (%d in total):' % len(gr.nonterms),
+        '\n'.join('\t' + str(s) for s in gr.nonterms) + '\n',
+        'Grammar terminals (%d in total):' % len(gr.terminals),
+        '\n'.join('\t' + str(s) for s in gr.terminals)
+    ])
+
+
+def describe_parsing_table(table):
+    conflict_status = table.get_conflict_status()
+
+    def conflict_status_str(state_id):
+        has_sr_conflict = (conflict_status[state_id] == lalr_one.STATUS_SR_CONFLICT)
+        status_str = ('shift-reduce' if has_sr_conflict else 'reduce-reduce')
+        return 'State %d has a %s conflict' % (state_id, status_str)
+
+    return ''.join([
+        'PARSING TABLE SUMMARY\n',
+        'Is the given grammar LALR(1)? %s\n' % ('Yes' if table.is_lalr_one() else 'No'),
+        ''.join(conflict_status_str(sid) + '\n' for sid in range(table.n_states)
+                if conflict_status[sid] != lalr_one.STATUS_OK) + '\n',
+        table.stringify()
+    ])
+
+
+def main():
+    print('generating..')
+    gr = get_grammar()
+    table = lalr_one.ParsingTable(gr)
+    print("Your file is ready..")
+
+    output_filename = 'parsing-table'
+
+    with open(output_filename + '.txt', 'w') as textfile:
+        textfile.write(describe_grammar(gr))
+        textfile.write('\n\n')
+        textfile.write(describe_parsing_table(table))
+
+    table.save_to_csv(output_filename + '.csv')
+
+
+if __name__ == "__main__":
+    main()
